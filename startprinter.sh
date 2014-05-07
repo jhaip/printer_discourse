73796ebdbd608df275ec9895e258c1f3f646917c
jhaip <jhaip@mit.edu>
2014-05-07 18:09:01 -0400

    running with question

--- startprinter.sh
+++ startprinter.sh
@@ -3,5 +3,5 @@ SUBDOMAINNAME = "discourse$1"
 /home/pi/ngrok -subdomain $SUBDOMAINNAME 5000 &
 echo "opening tunnel at subdomain $SUBDOMAINNAME"
 source /home/pi/printer_discourse/bin/activate
-python /home/pi/printer_discourse/run.py &
+python /home/pi/printer_discourse/run.py $2 &
 echo "starting twilio service"

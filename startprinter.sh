0c03d3c58feef8697db285fa09536df54f0d4392
jhaip <jhaip@mit.edu>
2014-05-07 18:23:06 -0400

    chm

--- startprinter.sh
+++ startprinter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-question = $2
-SUBDOMAINNAME = "discourse"$1
+question=$2
+SUBDOMAINNAME="discourse"$1
 /home/pi/ngrok -subdomain $SUBDOMAINNAME 5000 &
 echo "opening tunnel at subdomain $SUBDOMAINNAME"
 source /home/pi/printer_discourse/bin/activate

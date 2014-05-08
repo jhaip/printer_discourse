591a7877e81b9294c2dcd8071e782e6e205f03f6
jhaip <jhaip@mit.edu>
2014-05-08 10:49:48 -0400

    no more arguments for startprinter

--- startprinter.sh
+++ startprinter.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-question=$2
-SUBDOMAINNAME="discourse"$1
+question="..."
+SUBDOMAINNAME="$(hostname)"
+
+if [ "$SUBDOMAINNAME" = "discourse8" ]; then
+    question="My best feature is"
+else if [ "$SUBDOMAINNAME" = "discourse10" ]; then
+    question="I want to change"
+else if [ "$SUBDOMAINNAME" = "discourse6" ]; then
+    question="I want you to know"
+else if [ "$SUBDOMAINNAME" = "discourse4" ]; then
+    question="I am afraid of"
+else if [ "$SUBDOMAINNAME" = "discourse0" ]; then
+    question="I identify with"
+else if [ "$SUBDOMAINNAME" = "discourse7" ]; then
+    question="I am grateful for"
+else if [ "$SUBDOMAINNAME" = "discourse5" ]; then
+    question="I am happiest when"
+else if [ "$SUBDOMAINNAME" = "discourse2" ]; then
+    question="I depend on"
+else
+    question="...."
+fi
+
 /home/pi/ngrok -subdomain $SUBDOMAINNAME 5000 &
 echo "opening tunnel at subdomain $SUBDOMAINNAME"
 source /home/pi/printer_discourse/bin/activate
